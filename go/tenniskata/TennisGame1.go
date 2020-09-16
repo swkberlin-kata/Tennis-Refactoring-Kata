8442822d63538acbd1971c7f9ddcefdf2a9aab3f
Cassondra Foesch <puellanivis@gmail.com>
2020-09-16 12:49:12 +0000

    mob next [ci-skip]

--- go/tenniskata/TennisGame1.go
+++ go/tenniskata/TennisGame1.go
@@ -8,65 +8,72 @@ type tennisGame1 struct {
 }
 
 func TennisGame1(player1Name string, player2Name string) TennisGame {
-	game := &tennisGame1{
+	return &tennisGame1{
 		player1Name: player1Name,
-		player2Name: player2Name}
-
-	return game
+		player2Name: player2Name,
+	}
 }
 
 func (game *tennisGame1) WonPoint(playerName string) {
-	if playerName == "player1" {
+	if playerName == game.player1Name {
 		game.m_score1 += 1
 	} else {
 		game.m_score2 += 1
 	}
 }
 
+var game1ScoreToName = map[int]string{
+	0: "Love",
+	1: "Fifteen",
+	2: "Thirty",
+}
+
 func (game *tennisGame1) GetScore() string {
-	score := ""
-	tempScore := 0
 	if game.m_score1 == game.m_score2 {
-		switch game.m_score1 {
-		case 0:
-			score = "Love-All"
-		case 1:
-			score = "Fifteen-All"
-		case 2:
-			score = "Thirty-All"
-		default:
-			score = "Deuce"
+		// There are special names for each of the scores where they are tied.
+
+		if name, ok := game1ScoreToName[game.m_score1]; ok {
+			return name + "-All"
 		}
-	} else if game.m_score1 >= 4 || game.m_score2 >= 4 {
+
+		return "Deuce"
+	}
+
+	if game.m_score1 >= 4 || game.m_score2 >= 4 {
 		minusResult := game.m_score1 - game.m_score2
+
 		if minusResult == 1 {
-			score = "Advantage player1"
-		} else if minusResult == -1 {
-			score = "Advantage player2"
-		} else if minusResult >= 2 {
-			score = "Win for player1"
-		} else {
-			score = "Win for player2"
+			return "Advantage player1"
 		}
-	} else {
-		for i := 1; i < 3; i++ {
-			if i == 1 {
-				tempScore = game.m_score1
-			} else {
-				score += "-"
-				tempScore = game.m_score2
-			}
-			switch tempScore {
-			case 0:
-				score += "Love"
-			case 1:
-				score += "Fifteen"
-			case 2:
-				score += "Thirty"
-			case 3:
-				score += "Forty"
-			}
+
+		if minusResult == -1 {
+			return "Advantage player2"
+		}
+
+		if minusResult >= 2 {
+			return "Win for player1"
+		}
+
+		return "Win for player2"
+	}
+
+	var score string
+	var tempScore int
+
+	for i := 1; i < 3; i++ {
+		if i == 1 {
+			tempScore = game.m_score1
+		} else {
+			score += "-"
+			tempScore = game.m_score2
+		}
+
+		if name, ok := game1ScoreToName[tempScore]; ok {
+			score += name
+		} else {
+			score += "Forty"
 		}
 	}
+
 	return score
 }
